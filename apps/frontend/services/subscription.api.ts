b4b319b1ec8d0ebcc54c3ae18c7f6db707baa256
GlaDrancE <ayushr16060@gmail.com>
2026-04-21 13:17:16 +0530

    update api endpoint for sub

--- apps/frontend/services/subscription.api.ts
+++ apps/frontend/services/subscription.api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3002/api/v1",
+    // baseURL: "https://medilink-h77v.onrender.com/api/v1",
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    timeout: 60000,
 });
 
 // Add token interceptor
